4f786cac82a7785c3d183e9a9b6dd9796f392cca
kleon0313@gmail.com <Alejandro>
2020-11-09 13:53:31 -0600

    añadido de notas en fila de 3

--- script.js
+++ script.js
@@ -2,41 +2,39 @@ window.onload = function(){
     //Oculta el modal
     document.getElementById("cancel").addEventListener("click",
     function(){
-        document.getElementById("mod").style = "display:none";
+        document.getElementById("mod").style.display = "none";
     })
     
     //Muestra el modal
-    document.getElementsByClassName("add")[0].addEventListener("click",
+    document.getElementById("add").addEventListener("click",
     function(){
-        document.getElementById("mod").style = "display:block";
+      alert("LOG")
+        document.getElementById("mod").style.display = "block";
     });
 
     //agrega una nueva nota
-
+    
     var contador = 1;
-    function save(){
-        //¿Se ha llenado una linea de tres columnas?
-        if(contador % 3 != 0 ){
-            document.getElementsByClassName("columns")[0].appendChild("Hello");
-        }
-    }
-
-}
-
-var contador = 1;
-
-function column_number(){
-    return col_length = document.getElementsByClassName("columns").length;
-}
-
-function save(){
-
-    //¿Se ha llenado una linea de tres columnas?
-    if(contador % 3 != 0 ){
-        document.getElementsByClassName("columns")[column_number()-1].innerHTML += "<div class='column is-4'><p>Hola</p></div>"
-    }
-    else {
+    var columna = 0;
+    
+    document.getElementById("save").addEventListener('click',
+    function(){
+    
+      //¿Se ha llenado una linea de tres columnas?
+      if (contador < 3) {
+        document.getElementsByClassName("columns")[columna].innerHTML += "<div class='column is-4'><p>Hola</p></div>"
+        contador++
+      }
+      else {
+        columna++
+        contador = 0
         document.getElementsByClassName("container")[0].innerHTML += "<div class='columns'></div>"
-        document.getElementsByClassName("columns")[col_length-1].innerHTML += "<div class='column is-4'><p>Hola</p></div>"
+        document.getElementsByClassName("columns")[columna].innerHTML += "<div class='column is-4'><p>Hola</p></div>"
+        contador++
+      }
+      document.getElementById("mod").style.display = "none";
     }
+    )
+    
+
 }
