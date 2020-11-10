bc27b2df2a1f71e94b44947e22001b7dd5ca4ca0
kleon0313@gmail.com <Alejandro>
2020-11-09 19:28:25 -0600

    agregado de notas completo

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ window.onload = function(){
     //Muestra el modal
     document.getElementById("add").addEventListener("click",
     function(){
-      alert("LOG")
         document.getElementById("mod").style.display = "block";
     });
 
@@ -19,20 +18,27 @@ window.onload = function(){
     
     document.getElementById("save").addEventListener('click',
     function(){
+    var titulo = document.getElementById("titulo").value
+    var texto = document.getElementById("texto").value
     
       //¿Se ha llenado una linea de tres columnas?
       if (contador < 3) {
-        document.getElementsByClassName("columns")[columna].innerHTML += "<div class='column is-4'><p>Hola</p></div>"
+        document.getElementsByClassName("columns")[columna].innerHTML += "<div class='column is-4'><div class='nota'><h1>"+titulo+"</h1><p>"+texto+"</p></div></div>"
         contador++
       }
       else {
         columna++
         contador = 0
         document.getElementsByClassName("container")[0].innerHTML += "<div class='columns'></div>"
-        document.getElementsByClassName("columns")[columna].innerHTML += "<div class='column is-4'><p>Hola</p></div>"
+        document.getElementsByClassName("columns")[columna].innerHTML += "<div class='column is-4'><div class='nota'><h1>"+titulo+"</h1><p>"+texto+"</p></div></div>"
         contador++
       }
       document.getElementById("mod").style.display = "none";
+      
+      document.getElementById("add").addEventListener("click",
+        function() {
+          document.getElementById("mod").style.display = "block";
+        });
     }
     )
     
